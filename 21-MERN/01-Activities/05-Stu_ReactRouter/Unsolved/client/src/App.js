4a3baa0c4722e1dea6625ef59f0ea83e8ce6d2f1
DwayneL <dr2@ameritech.net>
2020-02-15 12:48:59 -0600

    book changes

--- 21-MERN/01-Activities/05-Stu_ReactRouter/Unsolved/client/src/App.js
+++ 21-MERN/01-Activities/05-Stu_ReactRouter/Unsolved/client/src/App.js
@@ -3,19 +3,28 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 // The app will not render correctly until you setup a Route component.
 // Refer to the Basic Example documentation if you need to.
 // (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <div>
       <Nav />
       <Books />
+
+      <Switch>
+        <Route exact path ="/">
+          <Books />
+        </Route>
+        <Route exact path ="">
+          <NoMatch />
+        </Route>
+      </Switch>
     </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
